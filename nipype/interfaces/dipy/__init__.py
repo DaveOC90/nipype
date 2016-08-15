b75ac37120f9dcd43e6044e42fca37d135735c88
James Pooley <james.pooley@gmail.com>
2016-08-15 13:20:03 -0400

    WIP: update imports in __init__.py

--- nipype/interfaces/dipy/__init__.py
+++ nipype/interfaces/dipy/__init__.py
@@ -1,5 +1,5 @@
 from .tracks import StreamlineTractography, TrackDensityMap
-from .tensors import TensorMode, DTI
+from .tensors import TensorMode, DTI, DKI
 from .preprocess import Resample, Denoise
 from .reconstruction import RESTORE, EstimateResponseSH, CSD
 from .simulate import SimulateMultiTensor
